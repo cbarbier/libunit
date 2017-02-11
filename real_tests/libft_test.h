8d61bd8b05ec197604dcc911bffb92ef1367728b
Matthieu TIRELLI <matirell@e2r4p14.42.fr>
2017-02-11 19:23:14 +0100

    .h

---
+++ real_tests/libft_test.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_test.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matirell <matirell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/11 19:08:43 by matirell          #+#    #+#             */
+/*   Updated: 2017/02/11 19:11:18 by matirell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_TEST_H
+# define LIBFT_TEST_H
+# include <stdlib.h>
+# include <string.h>
+
+int	ft_strcmp(const char *s1, const char *s2);
+#endif
