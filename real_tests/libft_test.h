1d8b2cf6c80f6ef3d3a1cdff377a223fa153d00d
Cedric BARBIER <cbarbier@e2r4p13.42.fr>
2017-02-11 19:45:35 +0100

    libft test.h

--- real_tests/libft_test.h
+++ real_tests/libft_test.h
@@ -6,7 +6,7 @@
 /*   By: matirell <matirell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 19:08:43 by matirell          #+#    #+#             */
-/*   Updated: 2017/02/11 19:11:18 by matirell         ###   ########.fr       */
+/*   Updated: 2017/02/11 19:25:00 by cbarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@
 # include <string.h>
 
 int	ft_strcmp(const char *s1, const char *s2);
+int	one_char_string(void);
+int	tab_and_spaces(void);
+int	null_string(void);
+int	string_with_null(void);
+
 #endif
