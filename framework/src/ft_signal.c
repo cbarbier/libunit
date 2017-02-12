6565710bfdb781c2fcc40e45c7c4586368344d3f
Cedric BARBIER <cbarbier@e2r5p12.42.fr>
2017-02-12 20:29:14 +0100

    timeout

--- framework/src/ft_signal.c
+++ framework/src/ft_signal.c
@@ -6,7 +6,7 @@
 /*   By: cbarbier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/12 19:38:25 by cbarbier          #+#    #+#             */
-/*   Updated: 2017/02/12 20:09:03 by cbarbier         ###   ########.fr       */
+/*   Updated: 2017/02/12 20:18:09 by cbarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int		ft_putsignal(t_unit_test *tmp)
 			ft_putstr("\033[0;36mBUSE\033[0m");
 		else if (tmp->signal == SIGALRM)
 			ft_putstr("\033[45mTIMEOUT\033[0m");
+		else if (tmp->signal == SIGABRT)
+			ft_putstr("\033[43mABORT\033[0m");
 		else
 			ft_putstr("\033[0;31mKO\033[0m");
 		ft_putendl("]");
--- tests/framework-tests/00_launcher.c
+++ tests/framework-tests/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: matirell <matirell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/11 08:18:02 by matirell          #+#    #+#             */
-/*   Updated: 2017/02/12 20:14:20 by matirell         ###   ########.fr       */
+/*   Updated: 2017/02/12 20:28:08 by cbarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,16 @@ int		framework_launcher(void)
 	ft_putendl("\t\t{ ------------------------------- }");
 	ft_putendl("\t\t{ - 42 FRAMEWORK LIBUNIT TESTER - }");
 	ft_putendl("\t\t{ ------------------------------- }");
-	load_test(&tests, " 1/4 [EASY] - Mmh, like to say OK ?\t", \
+	load_test(&tests, " 1/5 [EASY] - Mmh, like to say OK ?\t", \
 	basic_test_success);
-	load_test(&tests, " 2/4 [EASY] - What happend on KO ?\t", \
+	load_test(&tests, " 2/5 [EASY] - What happend on KO ?\t", \
 	basic_test_error);
-	load_test(&tests, " 3/4 [HARD] - Can you catch a SIGSEGV ? ", \
+	load_test(&tests, " 3/5 [HARD] - Can you catch a SIGSEGV ? ", \
 	lets_segv);
-	load_test(&tests, " 4/4 [HARD] - Can you catch a SIGBUS ?  ", \
+	load_test(&tests, " 4/5 [HARD] - Can you catch a SIGBUS ?  ", \
 	lets_berror);
+	load_test(&tests, " 5/5 [HARD] - Can you catch a TIMEOUT ? ", \
+	lets_timeout);
 	tests.returnval = ft_execute_tests(&tests);
 	ft_display_resume(&tests, 1);
 	return (tests.returnval);
